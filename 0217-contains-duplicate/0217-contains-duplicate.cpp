123c2dba88b051eddbc941ce0737c880cb827221
Hamza Zafar <45764331+hamzafer@users.noreply.github.com>
2022-10-16 22:35:44 +0500

    Time: 296 ms (5.15%), Space: 56.8 MB (6.99%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,18 +1,20 @@
-class Solution {
-public:
-    set<int> convertToSet(vector<int> v)
+class Solution
 {
-    set<int> s;
-     for (int x : v) {
-        s.insert(x);
-    }
-     return s;
-}
-    
-    bool containsDuplicate(vector<int>& nums) {
+    public:
+        set<int> convertToSet(vector<int> v)
+        {
+            set<int> s;
+            for (int x: v)
+            {
+                s.insert(x);
+            }
+            return s;
+        }
 
-                if(convertToSet(nums).size() == nums.size())
-                    return false;
+    bool containsDuplicate(vector<int> &nums)
+    {
+        if (convertToSet(nums).size() == nums.size())
+            return false;
 
         return true;
     }
